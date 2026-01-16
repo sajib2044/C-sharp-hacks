4ec1692eab88b53008ff5ff41064c53f42d055fd
sajib <sajibg563@gmail.com>
2026-01-17 01:23:47 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_sharp_hacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
